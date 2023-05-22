cfc2fdde3ebe33a04486b9c6113a074616835be8
Stephany-Doris <stephany.awino@twiga.com>
2023-05-22 08:28:45 +0300

    react native jobs app initial setup

---
+++ app/index.js
@@ -0,0 +1,51 @@
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+import { COLORS, icons, images, SIZES } from "../constants";
+
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerTitle: "",
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
+              }
+            }}
+          />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
